d6b78ee61b17decc94d7cbac2a9ec85878a3fea4
vocamre <truemail@yandex.ru>
2012-03-07 02:39:52 +0300

    htacc

--- app/protected/config/main.php
+++ app/protected/config/main.php
@@ -38,16 +38,21 @@ return array(
 			'allowAutoLogin'=>true,
 		),
 		// uncomment the following to enable URLs in path-format
-		/*
-		'urlManager'=>array(
+		
+		/*'urlManager'=>array(
 			'urlFormat'=>'path',
+			'showScriptName'=>false ,
 			'rules'=>array(
-				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
+				/*'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+				 'admin' => 'site/login',
+				 'about'=>'site/page/view/about',
+				 'casting'=>'site/casting',
+				 'jury'=>'site/page/view/jury',
 			),
 		),
-		
+		/*
 		'db'=>array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 		),
